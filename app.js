0ebe81fa0896d7b4611389a58db917a65e334e9c
andy <andy@andys-MacBook-Pro.local>
2023-06-23 17:25:01 -0400

    try

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 const path = require("path");
 const app = express()
 // view engine setup
-app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 // #############################################################################
 // Logs all request paths and method
 app.use(function (req, res, next) {
@@ -17,15 +17,15 @@ app.use(function (req, res, next) {
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
 // listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('public', options))
+// var options = {
+//   dotfiles: 'ignore',
+//   etag: false,
+//   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+//   index: ['index.html'],
+//   maxAge: '1m',
+//   redirect: false
+// }
+// app.use(express.static('public', options))
 
 app.get('/', async (req, res) => {
     try {
